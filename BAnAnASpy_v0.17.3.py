a120af696b8ffdb61f9c66682dd86eeffd95d1ff
ChristopheOliveira <80535244+ChristopheOliveira@users.noreply.github.com>
2021-03-16 15:09:41 +0100

    Add files via upload
    minor corrections

--- BAnAnASpy_v0.17.3.py
+++ BAnAnASpy_v0.17.3.py
@@ -10,19 +10,21 @@
 # 'biopython' 'xlsxwriter'
 # Linux: pip install 'package' ou pip3 install 'package'
 # Windows: python ou py -m pip install 'package'
+
+# 'tkinter' if necessary
 # sudo apt-get install python-tk pour Python2.X
 # sudo apt-get install python3-tk pour Python3.X
 
 #####  Things to know about filename of SEQ  #####
-# Use '-'(6) to separate each part name
+# Use dash '-' to separate each part name
 # Fist part must be the patient id
 # Second part must be the gene
-# The other parts order is free
+# The order of other parts is free
 # The primer part must finish by 'F' or 'S' for forward sequences
 # The primer part must finish by 'R' for reverse sequences
-# For multiplex or partial exon names, separate each number by '_'(8) or '.' or '/'
-# Exp filenames: 1201645-ABCB4-8F, 1123546-ABCB11-DPN-8_9R
-# Exp files Ab1: A01_1201645-ABCB4-8F.ab1, H02_1123546-ABCB11-DPN-8_9R.ab1
+# For multiplex or partial exon names, separate each number by underscore '_' or dot '.' or slash '/'
+# Exp filenames before sequencing: 1201645-ABCB4-8F, 1123546-ABCB11-DPN-8_9R
+# Exp ab1 files: A01_1201645-ABCB4-8F.ab1, H02_1123546-ABCB11-DPN-8_9R.ab1
 
 import glob
 import os
